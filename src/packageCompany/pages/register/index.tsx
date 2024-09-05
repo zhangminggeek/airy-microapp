2d37fc5bdbe21c0382900d8e8320a29f6fbb0c3e
zhangming <qianzm2490@gmail.com>
2024-09-05 15:00:33 +0800

    fix: 修复邀请码未回填问题

--- src/packageCompany/pages/register/index.tsx
+++ src/packageCompany/pages/register/index.tsx
@@ -1,6 +1,8 @@
 import { Button, Form, Input } from '@nutui/nutui-react-taro';
 import { View } from '@tarojs/components';
 import Taro, { useDidShow } from '@tarojs/taro';
+import { isEmpty } from 'lodash';
+import { useState } from 'react';
 
 import styles from './index.module.scss';
 
@@ -13,19 +15,20 @@ import { parseJson, RouterUtil } from '@/utils';
 
 const Page = () => {
   const [form] = Form.useForm();
+  // 邀请码
+  const [invitationCode, setInvitationCode] = useState<string>();
 
   useDidShow(() => {
     // 检查是否存在邀请码
-    const invitationCode = Taro.getStorageSync(StorageKey.INVITATION_CODE);
-    if (invitationCode) {
-      form.setFieldsValue({ invitationCode });
+    const code = Taro.getStorageSync(StorageKey.INVITATION_CODE);
+    if (code) {
+      setInvitationCode(code);
     }
     // 如果从后面的步骤返回，要回填注册信息
     const info = parseJson<Partial<CompanyInfo>>(
       Taro.getStorageSync(StorageKey.COMPANY_RESIGTER_INFO),
-      undefined,
     );
-    if (info) {
+    if (info && !isEmpty(info)) {
       const { province, city, area, logo, ...rest } = info;
       form.setFieldsValue({
         ...rest,
@@ -47,6 +50,7 @@ const Page = () => {
       <View className={styles.content}>
         <Form
           className="form-large"
+          form={form}
           labelPosition="left"
           divider
           footer={
@@ -63,7 +67,16 @@ const Page = () => {
           onFinish={async (values) => {
             const { region, logo, ...rest } = values;
             const [province, city, area] = region;
-            const params = { ...rest, province, city, area, logo: logo?.[0] };
+            const params = {
+              ...rest,
+              province,
+              city,
+              area,
+              logo: logo?.[0],
+            };
+            if (invitationCode) {
+              params.invitationCode = invitationCode;
+            }
             // 保存到本地，等后面验证手机号流程通过后使用
             Taro.setStorageSync(
               StorageKey.COMPANY_RESIGTER_INFO,
@@ -76,6 +89,7 @@ const Page = () => {
             label="联系人"
             name="contacts"
             rules={[{ required: true, message: '请输入联系人' }]}
+            validateTrigger="onBlur"
           >
             <Input />
           </Form.Item>
@@ -86,6 +100,7 @@ const Page = () => {
               { required: true, message: '请输入手机号' },
               { pattern: /1\d{10}/, message: '请输入正确的手机号' },
             ]}
+            validateTrigger="onBlur"
           >
             <Input />
           </Form.Item>
@@ -93,6 +108,7 @@ const Page = () => {
             label="店铺名称"
             name="name"
             rules={[{ required: true, message: '请输入店铺名称' }]}
+            validateTrigger="onBlur"
           >
             <Input />
           </Form.Item>
@@ -100,6 +116,7 @@ const Page = () => {
             label="店铺地址"
             name="region"
             rules={[{ required: true, message: '请选择店铺地址' }]}
+            validateTrigger="onBlur"
           >
             <Picker.Region />
           </Form.Item>
@@ -107,6 +124,7 @@ const Page = () => {
             label="详细地址"
             name="address"
             rules={[{ required: true, message: '请输入详细地址' }]}
+            validateTrigger="onBlur"
           >
             <Input />
           </Form.Item>
@@ -114,6 +132,7 @@ const Page = () => {
             label="营业执照"
             name="licenses"
             rules={[{ required: true, message: '请上传营业执照' }]}
+            validateTrigger="onBlur"
           >
             <Upload
               extra={
@@ -136,12 +155,11 @@ const Page = () => {
               }
             />
           </Form.Item>
-
           <Form.Item label="店铺LOGO" name="logo">
             <Upload />
           </Form.Item>
-          <Form.Item label="邀请码" name="invitationCode">
-            <Input disabled />
+          <Form.Item label="邀请码">
+            <View>{invitationCode}</View>
           </Form.Item>
         </Form>
       </View>
