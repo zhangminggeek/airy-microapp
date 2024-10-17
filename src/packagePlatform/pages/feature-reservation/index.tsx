618695cdc722bee3fbf48700f3740f6040ab41b6
zhangming <qianzm2490@gmail.com>
2024-10-17 11:43:14 +0800

    fix: 修改产品字段枚举值

--- src/packagePlatform/pages/feature-reservation/index.tsx
+++ src/packagePlatform/pages/feature-reservation/index.tsx
@@ -49,7 +49,7 @@ const Page = () => {
           <View className={styles['title-text']}>轻松提升运营效率</View>
         </View>
         <View className={styles.desc}>
-          十月份上线，现在预约免费使用，体验所有强大功能！
+          十一月份上线，现在预约免费使用，体验所有强大功能！
         </View>
         <Image
           className={styles.banner}
@@ -86,7 +86,7 @@ const Page = () => {
         </View>
         <View className={classnames(styles.section, styles.reservation)}>
           <View className={styles['reservation-title']}>
-            十月份上线，立即预约免费使用！
+            十一月份上线，立即预约免费使用！
           </View>
           <View className={styles['reservation-flow']}>
             <View className={styles['reservation-flow-title']}>预约流程</View>
@@ -113,7 +113,7 @@ const Page = () => {
                   value={3}
                   description={
                     <View>
-                      <View>十月正式上线</View>
+                      <View>十一月正式上线</View>
                       <View>解锁免费使用</View>
                     </View>
                   }
